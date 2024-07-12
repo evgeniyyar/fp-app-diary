e8bcb58ec5517cfd95fbad604364736daf4d0bbb
Evgeniy_Ogurtsov_desktop <eaogurtsov@gmail.com>
2024-07-12 12:17:26 +0300

    Update the Edit and Complete buttons

--- first_project/gui.py
+++ first_project/gui.py
@@ -1,25 +1,26 @@
 import functions
 import PySimpleGUI
 
-
 label = PySimpleGUI.Text("Type in a to-do")
 input_box = PySimpleGUI.InputText(tooltip="Enter todo", key="todo")
 button_add = PySimpleGUI.Button("Add")
 list_box = PySimpleGUI.Listbox(values=functions.read_todos(),
                                key='todos', enable_events=True,
-                               size=[50, 10])
+                               size=[30, 10])
 button_edit = PySimpleGUI.Button("Edit")
 button_complete = PySimpleGUI.Button("Complete")
+button_exit = PySimpleGUI.Button("Exit")
 
 window = PySimpleGUI.Window('My To-Do App',
-                            layout=[[label, input_box, button_add], [list_box, button_edit, button_complete]],
+                            layout=[[label, input_box, button_add],
+                                    [list_box, button_edit, button_complete, button_exit]],
                             font=('Italic', 14))
 
 while True:
     event, values = window.read()
-    print("events:", event)
+    print("событие:", event)
     print(type(event))
-    print("values", values)
+    print("значение", values)
     print(type(values))
     match event:
         case 'Add':
@@ -43,15 +44,19 @@ while True:
             todo_to_complete = values['todos'][0]
 
             todos = functions.read_todos()
-            index = todos.index(todo_to_complete)
-            todos.pop(index)
+            #index = todos.index(todo_to_complete)
+            todos.remove(todo_to_complete)
             functions.write_todos(todos)
             window['todos'].update(values=todos)
+            window['todo'].update(value='')
 
         case 'todos':
-            window['todo'].update(value=values['todos'][0])
+            window['todo'].update(value=values['todos'][0].strip('\n'))
 
         case PySimpleGUI.WINDOW_CLOSED:
+            break
+
+        case 'Exit':
             break
 
 window.close()
